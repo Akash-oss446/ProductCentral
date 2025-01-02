f7447a40f4fc54d5b8f8d343e15f8e73084be765
Akash Kumar Prasad <71172002+Akash-oss446@users.noreply.github.com>
2025-01-02 22:36:45 +0530

    Update Productserviceimpl.java
    Implemented caching

--- productmanagebackend/src/main/java/com/example/demo/Service/Productserviceimpl.java
+++ productmanagebackend/src/main/java/com/example/demo/Service/Productserviceimpl.java
@@ -1,17 +1,23 @@
 package com.example.demo.Service;
 
 
-import com.example.demo.Repository.ProductRepository;
-import com.example.demo.product.Product;
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.example.demo.Repository.ProductRepository;
+import com.example.demo.product.Product;
 @Service
 public class Productserviceimpl implements  ProductService{
 @Autowired
    private ProductRepository productRepo;
     @Override
+    
     public Product saveProduct(Product p) {
         return productRepo.save(p);
     }
@@ -22,12 +28,18 @@ public class Productserviceimpl implements  ProductService{
     }
 
     @Override
+    @Cacheable(cacheNames="product",key="#id")
     public Product getProductById(Integer id) {
+        System.out.println("get product");
         return productRepo.findById(id).get();
     }
 
+    
+   
     @Override
+    @CacheEvict(cacheNames="product",key="#id")
     public String deleteProduct(Integer id) {
+        System.out.println("deleted");
         Product p1=productRepo.findById(id).get();
         if(p1!=null)
         {
@@ -38,6 +50,7 @@ public class Productserviceimpl implements  ProductService{
     }
 
     @Override
+    @CachePut(cacheNames="products",key="#product.id")
     public Product editProduct(Product p,Integer id) {
         Product oldp1=productRepo.findById(id).get();
         oldp1.setProductname(p.getProductname());
